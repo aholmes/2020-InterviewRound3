ccd5b08ee7351063fd8a4e2a57d44b36ed61f647
Aaron Holmes <aaron@aaronholmes.net>
2020-09-01 19:16:39 -0700

    Initial commit - starter console project

---
+++ UCLARound3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UCLARound3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
